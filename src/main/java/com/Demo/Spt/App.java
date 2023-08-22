4e3be03a208d79574fb424e053bb63019b27b8ef
Ranjith271 <ranjithkumar970627@gmail.com>
2023-08-22 11:48:45 +0530

    First Commit

---
+++ src/main/java/com/Demo/Spt/App.java
@@ -0,0 +1,13 @@
+package com.Demo.Spt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
